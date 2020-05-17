30fd6ddf5df32c22bfee1529af1bff8e1df5097f
susanavbarbosa <susanavbarbosa@users.noreply.github.com>
2020-05-17 15:43:02 +0100

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Weather from "./Weather";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           Coded by{" "}
           <a href="https://pt.linkedin.com/in/susanavbarbosa" target="_blank">
